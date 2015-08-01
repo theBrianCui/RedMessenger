a32fd4084f614c7f4ab341612042a68f2a3e57ba
Brian Cui <brianzcui@gmail.com>
2015-08-01 14:12:20 -0500

    Client: Added check for Socket.IO before initiating connection

--- client/redmessenger.js
+++ client/redmessenger.js
@@ -4,21 +4,26 @@ function RedMessenger(url, userID) {
     this.url = url;
     this.userID = userID;
 
-    var socket = io(this.url);
-    socket.on('connect', function() {
-        console.log('RedMessenger Connected!');
-        console.log('Sending identifier ' + userID);
-
-        socket.emit('identifier', this.userID);
-
-        socket.on('message', function(msg) {
-            this.onMessage(msg);
-        }.bind(this))
-    }.bind(this));
-
     this.onMessage =  function(msg) {
         console.log('RedMessage! ' + msg);
     };
+
+    var socket;
+    if(io) {
+        var socket = io(this.url);
+        socket.on('connect', function () {
+            console.log('RedMessenger Connected!');
+            console.log('Sending identifier ' + userID);
+
+            socket.emit('identifier', this.userID);
+
+            socket.on('message', function (msg) {
+                this.onMessage(msg);
+            }.bind(this))
+        }.bind(this));
+    } else {
+        console.log('Socket.IO is not embedded in this page. RedMessenger will not be able to serve messages.')
+    }
 }
 
 var rm = new RedMessenger('http://seanc-linux:8080/rm', 'brianc');
